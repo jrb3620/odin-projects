ff4e9d7c0e4809aa8cdbd07e66755d51d47a64e3
jrb3620 <jonathanbuckner@me.com>
2026-05-28 17:06:12 -0500

    feat: playgame and playround functions

--- project-2-rps/rps.js
+++ project-2-rps/rps.js
@@ -14,10 +14,68 @@ function getComputerChoice() {
 
 function getHumanChoice() {
     let b = prompt("select rock, paper or scissors")
-    return b
+    let c = b.toLowerCase()
+    return c
 }
 
-let computer_choice = getComputerChoice()
-let human_choice = getHumanChoice()
-console.log(computer_choice)
-console.log(human_choice)
+function playRound(human, computer) {
+    let answer = ""
+    if (human == "rock") {
+        if (computer == "rock"){
+            console.log(`Tie. You picked ${human} and they picked ${computer}`)
+        }
+        if (computer == "paper") {
+            console.log(`You lose. You picked ${human} and they picked ${computer}`)
+            computer_score += 1
+        }
+        if (computer == "scissors") {
+            console.log(`You win. You picked ${human} and they picked ${computer}`)
+            human_score += 1
+        }
+    }
+    if (human == "paper") {
+        if (computer == "rock"){
+            console.log(`You win. You picked ${human} and they picked ${computer}`)
+            human_score += 1
+        }
+        if (computer == "paper") {
+            console.log(`Tie. You picked ${human} and they picked ${computer}`)
+        }
+        if (computer == "scissors") {
+            console.log(`You lose. You picked ${human} and they picked ${computer}`)
+            computer_score += 1
+        }
+    }
+    if (human == "scissors") {
+        if (computer == "rock"){
+            console.log(`You lose. You picked ${human} and they picked ${computer}`)
+            computer_score += 1
+        }
+        if (computer == "paper") {
+            console.log(`You win. You picked ${human} and they picked ${computer}`)
+            human_score += 1
+        }
+        if (computer == "scissors") {
+            console.log(`Tie. You picked ${human} and they picked ${computer}`)
+        }
+    }
+}
+
+function playGame() {
+while(human_score < 2 && computer_score < 2) { // bo3
+    let computer_choice = getComputerChoice()
+    let human_choice = getHumanChoice()
+    playRound (human_choice, computer_choice)
+}
+
+if(human_score > computer_score) {
+    console.log("you win. awesome dude.")
+}
+else {
+    console.log("you lose. lol.")
+}
+}
+var human_score = 0
+var computer_score = 0
+
+playGame()
