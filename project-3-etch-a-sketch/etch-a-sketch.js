ccf4d2296d6eec985ee965d383a6e77a6edae921
jrb3620 <jonathanbuckner@me.com>
2026-06-02 13:14:09 -0500

    feat: adding create grid and reset buttons

--- project-3-etch-a-sketch/etch-a-sketch.js
+++ project-3-etch-a-sketch/etch-a-sketch.js
@@ -1,9 +1,31 @@
-const divContainer = document.querySelector("#div-container");
-for(let i = 0; i < 256; i++){
-    let div = document.createElement("div");
-    div.classList.add("div-grid");
-    div.addEventListener("mouseenter", () => {
-        div.style.backgroundColor = "white";
-    });
-    divContainer.appendChild(div);
+function createGrid(size) {
+    for(let i = 0; i < size; i++){
+        for (let j = 0; j < size; j++) {
+            let div = document.createElement("div");
+            div.classList.add("div-grid");
+            div.addEventListener("mouseenter", () => {
+                div.style.backgroundColor = "white";
+            });
+            divContainer.appendChild(div);
+        }
+    }
 }
+function resetGrid() {
+    divContainer.replaceChildren();
+}
+
+const resetButton = document.querySelector("#reset-button");
+const createGridButton = document.querySelector("#create-grid-button");
+    const divContainer = document.querySelector("#div-container");
+resetButton.addEventListener("click", () => {
+    resetGrid();
+});
+createGridButton.addEventListener("click", () => {
+    let input = prompt("Please enter the grid size (input x input size). Do not enter anything over 100.");
+    if(input >= 1 && input <= 100 ) {
+        createGrid(input);
+    }
+    else {
+        alert("Invalid input");
+    }
+});
