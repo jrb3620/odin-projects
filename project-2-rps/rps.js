479ad8f951769b3ad62b5cc6170f6087be2eda2d
jrb3620 <jonathanbuckner@me.com>
2026-06-02 11:39:17 -0500

    feat: adding winner detection + reset button

--- project-2-rps/rps.js
+++ project-2-rps/rps.js
@@ -22,41 +22,50 @@ function playRound(human, computer) {
     let answer = ""
     if (human == "rock") {
         if (computer == "rock"){
-            console.log(`Tie. You picked ${human} and they picked ${computer}`)
+            resultString.textContent= `Tie. You picked ${human} and they picked ${computer}`
+            resultContainer.appendChild(resultString);
         }
         if (computer == "paper") {
-            console.log(`You lose. You picked ${human} and they picked ${computer}`)
+            resultString.textContent = `You lose. You picked ${human} and they picked ${computer}`
+            resultContainer.appendChild(resultString);
             computer_score += 1
         }
         if (computer == "scissors") {
-            console.log(`You win. You picked ${human} and they picked ${computer}`)
+            resultString.textContent = `You win. You picked ${human} and they picked ${computer}`
+            resultContainer.appendChild(resultString);
             human_score += 1
         }
     }
     if (human == "paper") {
         if (computer == "rock"){
-            console.log(`You win. You picked ${human} and they picked ${computer}`)
+            resultString.textContent = `You win. You picked ${human} and they picked ${computer}`
+            resultContainer.appendChild(resultString);
             human_score += 1
         }
         if (computer == "paper") {
-            console.log(`Tie. You picked ${human} and they picked ${computer}`)
+            resultString.textContent = `Tie. You picked ${human} and they picked ${computer}`
+            resultContainer.appendChild(resultString);
         }
         if (computer == "scissors") {
-            console.log(`You lose. You picked ${human} and they picked ${computer}`)
+            resultString.textContent = `You lose. You picked ${human} and they picked ${computer}`
+            resultContainer.appendChild(resultString);
             computer_score += 1
         }
     }
     if (human == "scissors") {
         if (computer == "rock"){
-            console.log(`You lose. You picked ${human} and they picked ${computer}`)
+            resultString.textContent = `You lose. You picked ${human} and they picked ${computer}`
+            resultContainer.appendChild(resultString);
             computer_score += 1
         }
         if (computer == "paper") {
-            console.log(`You win. You picked ${human} and they picked ${computer}`)
+            resultString.textContent = `You win. You picked ${human} and they picked ${computer}`
+            resultContainer.appendChild(resultString);
             human_score += 1
         }
         if (computer == "scissors") {
-            console.log(`Tie. You picked ${human} and they picked ${computer}`)
+            resultString.textContent = `Tie. You picked ${human} and they picked ${computer}`
+            resultContainer.appendChild(resultString);
         }
     }
 }
@@ -66,43 +75,66 @@ function updateScore(player, computer) {
     scoreContainer.appendChild(humanScore);
     scoreContainer.appendChild(computerScore);
 }
+function resetScore() {
+    const resetButton = document.createElement("button", "reset");
+    resetButton.textContent = "reset";
+    winnerContainer.appendChild(resetButton);
+    resetButton.addEventListener("click", () => {
+        updateScore(0,0);
+        human_score = 0
+        computer_score = 0
+        winnerString.textContent = "";
+        winnerContainer.appendChild(winnerString);
+        resultString.textContent = "Click one of the three buttons to start RPS";
+        resultContainer.appendChild(resultString);
+        buttonContainer.style.display = "block";
+        resetButton.remove();
+    });
+}
+function checkScore(player, computer){
+    if(player >= 5) {
+        winnerString.textContent = "Human won the first to five!"
+        winnerContainer.appendChild(winnerString);
+        buttonContainer.style.display = "none";
+        resetScore();
+    }
+    if(computer >= 5) {
+        winnerString.textContent = "Computer won the first to five!"
+        winnerContainer.appendChild(winnerString);
+        buttonContainer.style.display = "none";
+        resetScore()
+    }
+}
 var human_score = 0
 var computer_score = 0
+const buttonContainer = document.querySelector("#btn-container");
 const rockButton = document.querySelector("#rock-btn");
 rockButton.addEventListener("click", () => {
     let computer_choice = getComputerChoice();
     playRound("rock", computer_choice);
     updateScore(human_score, computer_score)
+    checkScore(human_score, computer_score)
 });
 const paperButton = document.querySelector("#paper-btn");
 paperButton.addEventListener("click", () => {
     let computer_choice = getComputerChoice();
     playRound("paper", computer_choice);
     updateScore(human_score, computer_score)
+    checkScore(human_score, computer_score)
 });
 const scissorsButton = document.querySelector("#scissors-btn");
 scissorsButton.addEventListener("click", () => {
     let computer_choice = getComputerChoice();
     playRound("scissors", computer_choice);
     updateScore(human_score, computer_score)
+    checkScore(human_score, computer_score)
 });
 const scoreContainer = document.querySelector("#score-container");
 const humanScore = document.createElement("div");
 const computerScore = document.createElement("div");
-// function playGame() {
-// while(human_score < 2 && computer_score < 2) { // bo3
-//     let computer_choice = getComputerChoice()
-//     let human_choice = getHumanChoice()
-//     playRound (human_choice, computer_choice)
-// }
-
-// if(human_score > computer_score) {
-//     console.log("you win. awesome dude.")
-// }
-// else {
-//     console.log("you lose. lol.")
-// }
-// }
-
-
-// playGame()
+const resultContainer = document.querySelector("#result-container");
+const resultString = document.createElement("div");
+resultString.textContent = "Click one of the three buttons to start RPS"
+resultContainer.appendChild(resultString);
+const winnerContainer = document.querySelector("#winner-container");
+const winnerString = document.createElement("div");
